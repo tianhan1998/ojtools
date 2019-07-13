b7f13d0095bd68238de065adb46c3470cf9ebe18
涵 田 <892265525@qq.com>
2019-07-13 15:55:51 +0800

    增加新用户

--- src/main/java/servlet/LoginServlet.java
+++ src/main/java/servlet/LoginServlet.java
@@ -13,7 +13,7 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username=request.getParameter("username");
         request.getSession().setAttribute("username",username);
-        String name[]={"朱森林","丁世杰","田涵","闫兴东","王元帅","岳阳","姜兴轮","陈诗毅","黄智超","张浩","张振强","张雨欣","赵明柯","张鹏","李富龙","郭雨鹏","马海力","程钰杰"};
+        String name[]={"朱森林","丁世杰","田涵","闫兴东","王元帅","岳阳","姜兴轮","陈诗毅","黄智超","张浩","张振强","张雨欣","赵明柯","张鹏","李富龙","郭雨鹏","马海力","程钰杰","郭卓营"};
         for(int i=0;i<name.length;i++){
             if(username.equals(name[i])) {
                 if("true".equals(request.getParameter("autologin"))) {
